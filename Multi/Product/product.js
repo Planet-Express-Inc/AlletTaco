0eb2dd7b9529ae47396e6985a3620a1fbd94d4ea
Skycry805 <niware@gmx.de>
2025-06-11 17:59:26 +0200

    product finished

--- Multi/Product/product.js
+++ Multi/Product/product.js
@@ -9,33 +9,41 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (isNaN(productId)) return;
   
     try {
-        // 2. Holen der Produkt- und Verkäufer-Daten
-        // fetch('https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}'),
 
-        const [productRes, sellerRes] = await Promise.all([
-            console.log(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`),
-            console.log(fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`)),
-            fetch('/Multi/seller.json')
-        ]);
-  
-        const [product, seller] = await Promise.all([
-            productRes.json(),
-            sellerRes.json()
-        ]);
-        
-        console.log("Produkt:", product);
-        console.log("Verkäufer:", seller);
+        console.log(fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`));
+
+        // Produktdaten holen
+        const productRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`);
+        const productArray = await productRes.json();
+        const product = productArray[0];  
+
+        console.log("Produktdaten:", product);
+        const sellerId = product.verkaeufer_id;
+
+        // Verkäuferdaten holen
+        const sellerRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/info/${sellerId}`);
+        const sellerArray = await sellerRes.json();
+        const seller = sellerArray[0]; 
+        console.log("Verkäuferdaten:", seller);
+
+        const ratingRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/reviews/${sellerId}`);
+        const ratingArray = await ratingRes.json();
+        const summe = ratingArray.reduce((acc, ratingArray) => acc + ratingArray.sterne, 0);
+        const durchschnitt = summe / ratingArray.length;
+        const sternegerundet = Math.round(durchschnitt);
+
+        console.log("Durchschnittspreis:", durchschnitt)
   
         // 3. Füllen der Produktinformationen im HTML
-        document.getElementById('product-image').src = product.image_url;
-        document.getElementById('product-image').alt = product.title;
-        document.getElementById('preis').textContent = "Preis: " + product.price.replace(".",",") + "€";
-        document.getElementById('product-title').textContent = product.title;
-        document.getElementById('product-description').textContent = product.description;
+        document.getElementById('product-image').src = "/Multi/Bilder/RalfSchumacherWasMachstDuDennHier.jpg";
+        document.getElementById('product-image').alt = product.titel;
+        document.getElementById('preis').textContent = "Preis: " + product.preis.replace('.',',') + " €";
+        document.getElementById('product-title').textContent = product.titel;
+        document.getElementById('product-description').textContent = product.beschreibung;
   
-        document.getElementById('seller-name').textContent = "Name: " + seller.name;
-        document.getElementById('seller-rating').textContent = "Bewertung: " + seller.rating;
-        document.getElementById('seller-contact').textContent = "Email: " + seller.contact;
+        document.getElementById('seller-name').textContent = "Name: " + seller.benutzername;
+        document.getElementById('seller-rating').textContent = "Bewertung: " + sternegerundet;
+        document.getElementById('seller-contact').textContent = "Email: " + seller.email;
 
         const sellerBox = document.getElementById("seller-box");
         sellerBox.addEventListener('click', () => {
