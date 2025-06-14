b8eba284f68103fabedf61aa45bc201945428245
Torben <torben.hintzmann@googlemail.com>
2025-06-14 15:04:26 +0200

    IchCommitte

--- Multi/orderHistory/orderHistory.js
+++ Multi/orderHistory/orderHistory.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
    
         .then(response => response.json())
         .then(orders => {
-            console.log("Antwort von der API:", orders); //Fehersuche
+            // console.log("Antwort von der API:", orders); //Fehersuche
             const orderHistory = document.getElementById("orderHistory");
             orderHistory.innerHTML = ""; // Alte Inhalte löschen
 
@@ -34,25 +34,18 @@ document.addEventListener("DOMContentLoaded", function() {
                 const verkauferCall = await fetch(apiBaseUrl + "user/info/" + product.verkaeufer_id);
                 const verkauferArray = await verkauferCall.json();
                 const verkaufer = verkauferArray[0];
-                console.log("kauferCall-ID:", productArray); //Fehersuche
+                console.log("kauferCall-ID:", verkauferArray); //Fehersuche
                 orderDiv.innerHTML = `
                     <h3>Artikel: ${product ? product.titel : order.artikel_id}</h3>
                     <p>Kauf-ID: ${order.kauf_id}</p>
-                    <p>Verkäufer: ${verkaufer ? verkaufer.vorname + ' '+ verkaufer.nachname : verorder.kaeufer_id}</p>
+                    <p>Verkäufer: ${verkaufer ? verkaufer.benutzername  : verorder.kaeufer_id}</p>
                     <p>Preis: ${order.kaufpreis} €</p>
                     <p>Versand: ${order.versanddaten}</p>
-                `;
+                    <p><!-- Bewertung-Link -->
+                    <a href="/Multi/Bewerten/bewerten.html?seller_id=${verkaufer.benutzer_id}">Diese Bestellung bewerten</a>
+                    </p>`
                 orderHistory.appendChild(orderDiv);
             });
         })
         .catch(error => console.error("Fehler beim Laden der Bestellungen:", error));
-});
-
-fetch(apiBaseUrl+'system/status/api')
-            .then(istAllesTaco => istAllesTaco.text())
-            .then(data => {
-                document.getElementById('istAllesTaco').textContent = data;
-            })
-            .catch(error => {
-                document.getElementById('istAllesTaco').textContent = 'Fehler bei der Api' + error;
 });
