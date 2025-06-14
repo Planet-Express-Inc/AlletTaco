50f1d114ce8f2d3b134b3fc247ba61aa62b148eb
Torben <torben.hintzmann@googlemail.com>
2025-06-14 14:49:11 +0200

    IchBesellung

--- Multi/orderHistory/orderHistory.js
+++ Multi/orderHistory/orderHistory.js
@@ -12,7 +12,11 @@ window.addEventListener("click", function(event) {
 
 document.addEventListener("DOMContentLoaded", function() {
     // API-URL wie im Screenshot
-    fetch(apiBaseUrl +"user/purchase",)
+    fetch(apiBaseUrl +"user/purchase", {
+      method: 'GET',
+      credentials: 'include'}) // Cookies mit senden)}
+
+   
         .then(response => response.json())
         .then(orders => {
             console.log("Antwort von der API:", orders); //Fehersuche
@@ -20,14 +24,22 @@ document.addEventListener("DOMContentLoaded", function() {
             orderHistory.innerHTML = ""; // Alte Inhalte löschen
 
             // Gehe über alle Bestellungen im Array
-            orders.forEach(order => {
+            orders.forEach(async order => {
                 const orderDiv = document.createElement("div");
                 orderDiv.classList.add("order-item");
+                const productCall = await fetch(apiBaseUrl + "article/" + order.artikel_id);
+                const productArray = await productCall.json();
+                const product = productArray[0];
+
+                const verkauferCall = await fetch(apiBaseUrl + "user/info/" + product.verkaeufer_id);
+                const verkauferArray = await verkauferCall.json();
+                const verkaufer = verkauferArray[0];
+                console.log("kauferCall-ID:", productArray); //Fehersuche
                 orderDiv.innerHTML = `
-                    <h3>Artikel-ID: ${order.artikel_id}</h3>
-                    <p>Käufer-ID: ${order.kaeufer_id}</p>
+                    <h3>Artikel: ${product ? product.titel : order.artikel_id}</h3>
                     <p>Kauf-ID: ${order.kauf_id}</p>
-                    <p>Preis: ${order.kaupreis} €</p>
+                    <p>Verkäufer: ${verkaufer ? verkaufer.vorname + ' '+ verkaufer.nachname : verorder.kaeufer_id}</p>
+                    <p>Preis: ${order.kaufpreis} €</p>
                     <p>Versand: ${order.versanddaten}</p>
                 `;
                 orderHistory.appendChild(orderDiv);
