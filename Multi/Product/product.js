f182603fc99a24587d148ffd7127f3f266ed2168
Skycry805 <niware@gmx.de>
2025-06-15 11:13:44 +0200

    added views in product and verkaeufer

--- Multi/Product/product.js
+++ Multi/Product/product.js
@@ -3,59 +3,99 @@ const productId = parseInt(params.get('id'), 10);
 let seller;
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // 1. Holen der Produkt-ID aus der URL
-    const params = new URLSearchParams(window.location.search);
-    const productId = parseInt(params.get('id'), 10);
   
-    if (isNaN(productId)) return;
-  
-    try {
+  try {
 
-        console.log(fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`));
+    // Get already flown views
+    const viewsRes = await fetch (`https://allestaco.niclas-sieveneck.de:5000/v1/article/views/${productId}`);
+    const viewArray = await viewsRes.json();
+    const view = viewArray[0];
+    const user = sessionStorage.getItem("user_id")
 
-        // Produktdaten holen
-        const productRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`);
-        const productArray = await productRes.json();
-        const product = productArray[0];  
+    // If first view
+    let anzahl = 0;
+    if (typeof view == 'undefined') {
+      anzahl = 1;
+      console.log("undefiniert")
+    }
+    else{
+      anzahl = parseInt(view.anzahl,10) + 1;
+    }
+    
+    // Increase views
+      const productData = {
+        anzahl: anzahl,
+        artikel_id: productId,
+        aufrufer_id: user
+      };
+    console.log(productData);
 
-        console.log("Produktdaten:", product);
-        const sellerId = product.verkaeufer_id;
+    // Remove old views
+    await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/views/${productId}`,{
+      method: 'DELETE',
+      credentials: 'include'
+    })
 
-        // Verkäuferdaten holen
-        const sellerRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/info/${sellerId}`);
-        const sellerArray = await sellerRes.json();
-        seller = sellerArray[0]; 
-        console.log("Verkäuferdaten:", seller);
+    // Set new views
+    await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/views`,{
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+      body: JSON.stringify(productData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Hinzugefügt:', data);
+    })
+    .catch(error => {
+      console.error('Fehler beim Senden:', error);
+    });
 
-        const ratingRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/reviews/${sellerId}`);
-        const ratingArray = await ratingRes.json();
-        const summe = ratingArray.reduce((acc, ratingArray) => acc + ratingArray.sterne, 0);
-        const durchschnitt = summe / ratingArray.length;
-        const sternegerundet = Math.round(durchschnitt);
-        console.log("Durchschnittspreis:", durchschnitt)
-  
-        // 3. Füllen der Produktinformationen im HTML
-        document.getElementById('product-image').src = `https://allestaco.niclas-sieveneck.de:5000/v1/article/picture/${productId}`;
-        document.getElementById('product-image').alt = product.titel;
-        document.getElementById('preis').textContent = "Preis: " + product.preis.replace('.',',') + " €";
-        document.getElementById('product-title').textContent = product.titel;
-        document.getElementById('product-description').textContent = product.beschreibung;
-  
-        document.getElementById('seller-name').textContent = "Name: " + seller.benutzername;
-        document.getElementById('seller-rating').textContent = "Bewertung: " + sternegerundet;
-        document.getElementById('seller-contact').textContent = "Email: " + seller.email;
+    // Get the product data
+    const productRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/${productId}`);
+    const productArray = await productRes.json();
+    const product = productArray[0];  
 
-        const sellerBox = document.getElementById("seller-box");
-        sellerBox.addEventListener('click', () => {
-          window.location.href = `/Multi/verkaeuferProfil/verkaeuferProfil.html?user_id=${seller.benutzer_id}`;
-        });
+    // Get the seller data
+    const sellerId = product.verkaeufer_id;
+    const sellerRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/info/${sellerId}`);
+    const sellerArray = await sellerRes.json();
+    seller = sellerArray[0]; 
+    console.log("Verkäuferdaten:", seller);
+
+    // Get the sellers rating
+    const ratingRes = await fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/user/reviews/${sellerId}`);
+    const ratingArray = await ratingRes.json();
+    const summe = ratingArray.reduce((acc, ratingArray) => acc + ratingArray.sterne, 0);
+    const durchschnitt = summe / ratingArray.length;
+    const sternegerundet = Math.round(durchschnitt);
+    console.log("Durchschnittspreis:", durchschnitt)
+
+    // Show the results on DOM
+    document.getElementById('product-image').src = `https://allestaco.niclas-sieveneck.de:5000/v1/article/picture/${productId}`;
+    document.getElementById('product-image').alt = product.titel;
+    document.getElementById('preis').textContent = "Preis: " + product.preis.replace('.',',') + " €";
+    document.getElementById('views').textContent = "Aufrufe: " + anzahl;
+    document.getElementById('product-title').textContent = product.titel;
+    document.getElementById('product-description').textContent = product.beschreibung;
+
+    document.getElementById('seller-name').textContent = "Name: " + seller.benutzername;
+    document.getElementById('seller-rating').textContent = "Bewertung: " + sternegerundet;
+    document.getElementById('seller-contact').textContent = "Email: " + seller.email;
+
+    const sellerBox = document.getElementById("seller-box");
+    sellerBox.addEventListener('click', () => {
+      window.location.href = `/Multi/verkaeuferProfil/verkaeuferProfil.html?user_id=${seller.benutzer_id}`;
+    });
         
     } catch (error) {
         console.error("Fehler beim Laden:", error);
     }
 
 });
-
+// Add products to the shoping cart
 function addToCart(){
     const userId = parseInt(sessionStorage.getItem('user_id'), 10);
       const productData = {
--- Multi/Verkaeufer/verkaeufer.js
+++ Multi/Verkaeufer/verkaeufer.js
@@ -1,70 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {    /*laden der Daten nach laden der Website */
-    
-    function loadArticles () {
-      const userId = parseInt(sessionStorage.getItem('user_id'), 10);
-        fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/user/${userId}`)  /*URL der API*/
-        .then(response => response.json())
-        .then(data => {
-            const list = document.getElementById('product-list');
-            /*auslesen der JSON*/
-            data.forEach(product => {
-            const item = document.createElement('div');
-            item.className = 'product';           
+document.addEventListener("DOMContentLoaded", () => { 
+  // Load all articels from the seller       
+  function loadArticles () {
+    const userId = parseInt(sessionStorage.getItem('user_id'), 10);
+    // Get all articel of the login user
+    fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/user/${userId}`)  
+      .then(response => response.json())
+      .then(data => {
+        const list = document.getElementById('product-list');
+        data.forEach(product => {
+          const item = document.createElement('div');
+          item.className = 'product';
+          console.log(product);           
 
-            /*hinzufügen der HTML Elemente in die vorhandene Website*/
-            item.innerHTML = `
+          // Get the views of an articel
+          fetch(`https://allestaco.niclas-sieveneck.de:5000/v1/article/views/${product.artikel_id}`)
+            .then(views => views.json())
+            .then(viewArray => {
+              let aufrufe = 0;
+              // Check if the views are 0
+              if (viewArray.length > 0) {
+                aufrufe = viewArray[0].anzahl;
+              } else {
+                aufrufe = 0;  
+              }
+
+              // Build new html Elements with the articels
+              item.innerHTML = `
                 <img src="https://allestaco.niclas-sieveneck.de:5000/v1/article/picture/${product.artikel_id}" alt="${product.titel}">   
                 <div class="product-info">
-                <h3 class="product-title">${product.titel}</h3>
-                <p class="product-description">${product.beschreibung}</p>
-                <p class="product-price">Preis: ${product.preis.replace('.', ',')} €</p>
-                <p class="product-amount">Anzahl: ${product.bestand}</p>
-                <button onclick="adjust(${product.artikel_id})" class="adjust">Bearbeiten</button>
+                  <h3 class="product-title">${product.titel}</h3>
+                  <p class="product-description">${product.beschreibung}</p>
+                  <p class="product-price">Preis: ${product.preis.replace('.', ',')} €</p>
+                  <p class="product-amount">Bestand: ${product.bestand}</p>
+                  <p class="product-amount">Aufrufe: ${aufrufe}</p> 
+                  <button onclick="adjust(${product.artikel_id})" class="adjust">Bearbeiten</button>
                 </div>
-            `;
+              `;
 
-            item.addEventListener('click', () => {
+              item.addEventListener('click', () => {
                 window.location.href = `/Multi/editProduct/editProduct.html?articelid=${product.artikel_id}`;
-            });
-            /*unten anhängen */
-            list.appendChild(item);
-            });
-                const message = document.createElement('div');
-                message.className = 'no-products-clickable';
-                message.innerHTML = `
-                    <p>Neue Produkte hinzufügen <span class="plus-icon">+</span> </p>
-                `;
+              });
 
-                message.addEventListener('click', () => {
-                    window.location.href = '/Multi/newProduct/newProduct.html';
-                });
-
-                list.appendChild(message);
-                return;
-        })
-        .catch(err => {
-            console.error('Fehler beim Laden der Produkte:', err);
+              list.appendChild(item);
+            })
+            .catch(err => {
+              console.error('Fehler beim Laden der Views:', err);
+            });
         });
-    }
-    loadArticles();
-    userData();
-  });
 
-function userData (){
-  // Werte aus dem Session Storage holen
-      const username = sessionStorage.getItem('username');
-      const role = sessionStorage.getItem('roll');
+        // Field to add new articels
+        const message = document.createElement('div');
+        message.className = 'no-products-clickable';
+        message.innerHTML = `
+          <p>Neue Produkte hinzufügen <span class="plus-icon">+</span> </p>
+        `;
+        message.addEventListener('click', () => {
+          window.location.href = '/Multi/newProduct/newProduct.html';
+        });
+        list.appendChild(message);
+      })
+      .catch(err => {
+        console.error('Fehler beim Laden der Produkte:', err);
+      });
+  }
 
-      // DOM-Elemente holen
-      const userInfoEl = document.getElementById('user-info');
-      const userRoleEl = document.getElementById('user-role');
-
-      // Inhalte setzen
-      if (username && userInfoEl) {
-        userInfoEl.textContent = 'Angemeldet als: ' + username;
-      }
-
-      if (role && userRoleEl) {
-        userRoleEl.textContent = `(${role})`;
-      }
-}
+  loadArticles();
+});
